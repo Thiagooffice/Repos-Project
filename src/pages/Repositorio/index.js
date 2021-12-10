8354128e007a785ce15ae3af12df7f59dd700882
Thiago <thiagooffice2@gmail.com>
2021-12-09 22:45:56 -0300

    Criando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,9 +1,61 @@
+import {Container, Owner, Loading, BackButton} from './styles'
+import {useState, useEffect} from 'react'
+import api from '../services/api'
+import {FaArrowLeft} from 'react-icons/fa'
+
 
 
 export default function Repositorio({match}){
+
+    const [repositorio, setRepositorio]=useState({})
+    const [issues, setIssues]=useState([])
+    const [loading, setLoading]=useState(true)
+
+    useEffect(()=>{
+    async function load(){
+        const nomeRepo = decodeURIComponent( match.params.repositorio)
+
+    const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`,{
+            params:{
+                state: "open",
+                per_page: 5
+            }
+        })
+    ])
+    setRepositorio(repositorioData.data)
+    setIssues(issuesData.data)
+    setLoading(false)
+    }
+
+    load()
+
+
+    },[match.params.repositorio])
+
+    if(loading){
+        return(
+            <Loading>
+                <h1>Carregando.</h1>
+            
+            </Loading>
+        )
+    }
+    
     return(
-        <h1 style={{color: "#fff"}}>
-        {decodeURIComponent( match.params.repositorio)}
-        </h1>
+    <Container>
+        <BackButton to="/">
+             <FaArrowLeft color="#000" size={35}/>
+        </BackButton>
+    <Owner>
+        <img 
+        src={repositorio.owner.avatar_url} 
+        alt={repositorio.owner.login}
+        />   
+        <h1>{repositorio.name}</h1> 
+        <p>{repositorio.description}</p>
+    </Owner> 
+     </Container>
     )
 }
